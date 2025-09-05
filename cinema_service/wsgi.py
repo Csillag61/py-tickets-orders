04f881c3a3e013263f96b1eb8dffbbb66485b876
Csilla <csillagok@gmail.com>
2025-09-04 20:29:07 -0400

    flake8 errors fixed, ah

--- cinema_service/wsgi.py
+++ cinema_service/wsgi.py
@@ -12,8 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
-	"DJANGO_SETTINGS_MODULE",
-	"cinema_service.settings"
+    "DJANGO_SETTINGS_MODULE",
+    "cinema_service.settings"
 )
 
 application = get_wsgi_application()
